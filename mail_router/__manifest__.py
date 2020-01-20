dd12fbe2f45d5cb74e50687f774978b991e07071
Александр <triplustri@mail.ru>
2020-01-21 03:04:09 +0400

    Add index.html and icon.png

--- mail_router/__manifest__.py
+++ mail_router/__manifest__.py
@@ -16,9 +16,6 @@
     ],
     'license': 'OPL-1',
     'description': """
-        The module allows you to configure advanced routing of mail messages
-        to create entries in the Odoo database. Features provided by the standard module "mail" to create entries may not be enough.
-        The module allows you to extract the necessary information from the letters through the flexible settings system
-        and save it in the database as model records.
+        Advanced routing of mail messages
     """,
 }
